1c4d6ad564867f7b4eee5e5b778d3b162dcc86d4
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-03-06 12:25:09 +0100

    Created the data access object

---
+++ app/src/main/java/com/slepnev/shoppinglist/ProductDao.kt
@@ -0,0 +1,22 @@
+package com.slepnev.shoppinglist
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface ProductDao {
+
+    @Query("SELECT * FROM productTable")
+    suspend fun getAllProducts(): List<Product>
+
+    @Insert
+    suspend fun insertProduct(product: Product)
+
+    @Delete
+    suspend fun deleteProduct(product: Product)
+
+    @Query("DELETE FROM productTable")
+    suspend fun deleteAllProducts()
+}

427efc3c5e14b77d15a44d48df9ff4d141ece2b5
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-03-06 12:16:09 +0100

    Created the data class & db entity

---
+++ app/src/main/java/com/slepnev/shoppinglist/Product.kt
@@ -0,0 +1,21 @@
+package com.slepnev.shoppinglist
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "productTable")
+data class Product(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long? = null,
+
+    @ColumnInfo(name = "name")
+    var name: String,
+
+    @ColumnInfo(name = "quantity")
+    var quantity: Int
+    ) : Parcelable

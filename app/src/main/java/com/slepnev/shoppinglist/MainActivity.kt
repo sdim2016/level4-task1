c5a1097bd2f82a8465feef77f7f5b2a34eb4be06
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-03-06 13:37:15 +0100

    Created ProductAdapter and item_product layout

--- app/src/main/java/com/slepnev/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/slepnev/shoppinglist/MainActivity.kt
@@ -10,15 +10,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var productRepository: ProductRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        productRepository = ProductRepository(this)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/com/slepnev/shoppinglist/ProductAdapter.kt
@@ -0,0 +1,32 @@
+package com.slepnev.shoppinglist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_product.view.*
+
+class ProductAdapter(private val products:List<Product>):
+RecyclerView.Adapter<ProductAdapter.ViewHolder>()
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_product, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return products.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(products[position])
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(product: Product) {
+            itemView.tvProductName.text = product.name
+            itemView.tvProductCount.text = product.quantity.toString()
+        }
+    }
+}

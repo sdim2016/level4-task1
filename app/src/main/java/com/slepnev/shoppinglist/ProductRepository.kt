2a539eb2b1dcd447743572be94a549b57e44c456
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-03-06 12:40:46 +0100

    Created the repository

---
+++ app/src/main/java/com/slepnev/shoppinglist/ProductRepository.kt
@@ -0,0 +1,28 @@
+package com.slepnev.shoppinglist
+
+import android.content.Context
+
+class ProductRepository(context: Context) {
+    private val productDao: ProductDao
+
+    init {
+        val database = ShoppingListRoomDatabase.getDatabase(context)
+        productDao = database!!.productDao()
+
+        suspend fun getAllProducts(): List<Product> {
+            return productDao.getAllProducts()
+        }
+
+        suspend fun insertProduct(product: Product) {
+            return productDao.insertProduct(product)
+        }
+
+        suspend fun deleteProduct(product: Product) {
+            return productDao.deleteProduct(product)
+        }
+
+        suspend fun deleteAllProducts() {
+            return productDao.deleteAllProducts()
+        }
+    }
+}
